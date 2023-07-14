1e38b5088e4f0c4f845803442240969aab94e91f
Pantagruel74 <tostar74@mail.ru>
2023-07-15 00:05:44 +0500

    init project

---
+++ src/uralsclj_core/widget.clj
@@ -0,0 +1,6 @@
+(ns uralsclj-core.widget)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/uralsclj_core/widget_test.clj
@@ -0,0 +1,7 @@
+(ns uralsclj-core.widget-test
+  (:require [clojure.test :refer :all]
+            [uralsclj-core.widget :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
