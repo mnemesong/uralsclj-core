c1b433652260fbbd203b2fb1871d6b0cb14d4812
Pantagruel74 <tostar74@mail.ru>
2023-07-15 01:49:57 +0500

    add types

--- src/uralsclj_core/widget.clj
+++ src/uralsclj_core/widget.clj
@@ -1,4 +1,16 @@
-(ns uralsclj-core.widget)
+(ns uralsclj-core.widget
+  (:require [clojure.spec.alpha :as s]))
+
+(defn source? [x] (or (string? x) (coll? x)))
+
+(s/def ::html source?)
+(s/def ::js source?)
+(s/def ::css source?)
+(s/def ::html-engine string?)
+(s/def ::css-engine string?)
+(s/def ::js-engine string?)
+(s/def ::dsl 
+  (s/keys :opt [::html ::js ::css ::html-engine ::css-engine ::js-engine]))
 
 (defn foo
   "I don't do a whole lot."
--- test/uralsclj_core/widget_test.clj
+++ test/uralsclj_core/widget_test.clj
@@ -1,7 +1,21 @@
 (ns uralsclj-core.widget-test
   (:require [clojure.test :refer :all]
-            [uralsclj-core.widget :refer :all]))
+            [uralsclj-core.widget :refer :all]
+            [clojure.spec.alpha :as s]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest widget-dsl-test
+  (testing "Widget dsl empty"
+    (is (= true (s/valid? :uralsclj-core.widget/dsl {}))))
+  (testing "Widget dsl contains css-collection"
+    (is (= true (s/valid? :uralsclj-core.widget/dsl {:css ["hello"]}))))
+  (testing "Widget dsl contains css-string"
+    (is (= true (s/valid? :uralsclj-core.widget/dsl {:css "hello"}))))
+  (testing "Widget dsl contains js-collection"
+    (is (= true (s/valid? :uralsclj-core.widget/dsl {:js ["hello"]}))))
+  (testing "Widget dsl contains js-string"
+    (is (= true (s/valid? :uralsclj-core.widget/dsl {:js "hello"}))))
+  (testing "Widget dsl contains html-collection"
+    (is (= true (s/valid? :uralsclj-core.widget/dsl {:html ["hello"]}))))
+  (testing "Widget dsl contains html-string"
+    (is (= true (s/valid? :uralsclj-core.widget/dsl {:html "hello"})))))
+
